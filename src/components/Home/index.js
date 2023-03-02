e150f77732fc1f27fdf82c17aee8e542df08fdb9
Mauro Metz <maurometzdasilva@ienh.com.br>
2023-03-02 16:53:28 -0300

    commit

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,23 +1,59 @@
 import LogoTitle from '../../assets/images/logo-s.png'
 import { Link } from 'react-router-dom'
 import './index.scss'
+import { useEffect, useState } from 'react'
+import AnimatedLetters from '../AnimatedLetters'
 
 const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = ['a', 'u', 'r', 'o']
+    const jobArray = [
+        'w',
+        'e',
+        'b',
+        ' ',
+        'd',
+        'e',
+        'v',
+        'e',
+        'l',
+        'o',
+        'p',
+        'e',
+        'r',
+        '.',
+      ]
 
+    useEffect(() => {
+        return setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, 4000)
+    }, [])
 
     return (
-        <div className="container home-page">
-            <div className="text-zone">
-                <h1>Olá, <br /> Eu sou
-                <img src={LogoTitle} alt="developer" />
-                auro
-                <br />
-                web developer
-                </h1>
-                <h2>Fullstack Developer / React / Node / PHP</h2>
-                <Link to="/contact" className="flat-button">FALE COMIGO</Link>
+        <>
+            <div className="container home-page">
+                <div className="text-zone">
+                    <h1>
+                        <span className={letterClass}>Olá</span>
+                        <br />
+                        <span className={`${letterClass} _12`}>Eu </span>
+                        <span className={`${letterClass} _13`}>sou</span>
+
+                        <img src={LogoTitle} alt="developer" />
+                        <AnimatedLetters letterClass={letterClass}
+                            strArray={nameArray}
+                            idx={15} />
+                        <br />
+                        <AnimatedLetters letterClass={letterClass}
+                            strArray={jobArray}
+                            idx={22} />
+                    </h1>
+                    <h2>Fullstack Developer / React / Node / PHP</h2>
+                    <Link to="/contact" className="flat-button">FALE COMIGO</Link>
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 
